dd78fb38f39c208f0cbb87ed9190efcd316cf33c
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2024-02-08 02:05:06 +0100

    Ajout de commentaires

--- src/main/java/cda/annuaire/dto/user/UserDTO.java
+++ src/main/java/cda/annuaire/dto/user/UserDTO.java
@@ -5,10 +5,33 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserDTO {
 
+    /**
+     * Identifiant de l'utilisateur
+     */
     private long id;
+
+    /**
+     * Prénom de l'utilisateur
+     */
     private String firstname;
+
+    /**
+     * Nom de l'utilisateur
+     */
     private String lastname;
+
+    /**
+     * Adresse de l'utilisateur
+     */
     private String address;
+
+    /**
+     * Code postal de l'utilisateur
+     */
     private String zipCode;
+
+    /**
+     * Ville de l'utilisateur
+     */
     private String city;
 }
